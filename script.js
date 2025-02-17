8b8ef1840da66f4c60f9c176e662fd711b0d429a
intxaurtietadev <t.intxaurtietadev@gmail.com>
2025-02-17 13:52:37 +0100

    primeros cambios modo dark light

--- script.js
+++ script.js
@@ -4,3 +4,25 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+
+  //dark/light
+const toggleButton = document.getElementById("theme-toggle");
+toggleButton.addEventListener("click", () => {
+  const html = document.documentElement;
+  if (html.getAttribute("data-theme") === "dark") {
+    html.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+    toggleButton.textContent = "🌙";
+  } else {
+    html.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+    toggleButton.textContent = "☀️";
+  }
+});
+
+// Mantener la preferencia de tema guardada
+if (localStorage.getItem("theme") === "dark") {
+  document.documentElement.setAttribute("data-theme", "dark");
+  toggleButton.textContent = "☀️";
+}
